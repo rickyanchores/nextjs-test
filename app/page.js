d7830587c51d366cef5c6b0dd5516d3cf905c0b8
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-12-23 20:31:17 +0000

    first commit + jsx hero

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Hero from '@/pages/Hero/Hero'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="Home w-2/3 m-auto bg-slate-600">
+      <Hero />
+      hi test
+    </main>
+  )
+}
---
+++ pages/Hero/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='Hero bg-red-600 h-20'>Hero</div>
+  )
+}
+
+export default Hero;
