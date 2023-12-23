fce2a555d258f954779f8b6a230d8bdc1a91f8dd
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-12-23 20:54:41 +0000

    edit hero

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="Home w-2/3 m-auto bg-slate-600">
+    <main className="Home mr-5 ml-5 bg-slate-600">
       <Hero />
       hi test
     </main>
--- pages/Hero/Hero.jsx
+++ pages/Hero/Hero.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='Hero bg-red-600 h-20'>Hero</div>
+    <div className='Hero bg-slate-800 p-5 font-mono'>
+      <h1 className='Hero-title'>Test</h1>
+      <img src="https://preview.redd.it/hot-take-maybe-geto-is-still-the-best-written-character-in-v0-20srhwds9agb1.jpg?width=640&crop=smart&auto=webp&s=405bd569a977a0d4e7d70cbc0e125288f6eec392" alt="hero-img" />
+      <div className="text-container">
+        <h3>Suguru Geto</h3>
+        <p>Powerful sorcerer with ability to manipulate and absorb curses</p>
+      </div>
+    </div>
   )
 }
 
